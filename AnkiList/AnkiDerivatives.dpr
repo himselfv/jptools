fe09572316c1af8cd27f21d20fe58441a121a065
himselfv@gmail.com <himselfv@gmail.com>
2014-05-13 17:03:53 +0400

    AnkiDerivatives working (no EDICT resolution yet).

--- AnkiList/AnkiDerivatives.dpr
+++ AnkiList/AnkiDerivatives.dpr
@@ -1,11 +1,22 @@
 program AnkiMergeTags;
 {
 Parses a file listing derivative links between words:
-  base deriv deriv
-  base deriv
+  base deriv deriv "comment" deriv "comment"
 
 And produces a tab-separated file with comments about these relations between
 entries, to be imported to Anki.
+
+E.g.:
+  a b c d "comment"
+  a e
+  b e
+=>
+  a: Derivatives: b, c, d (comment).
+  b: From: a (comment); derivatives: e.
+  c, d: From: a (comment).
+  e: From: a (comment); b.
+
+TODO: Dict resolution.
 }
 
 {$APPTYPE CONSOLE}
@@ -13,16 +24,23 @@ entries, to be imported to Anki.
 {$R *.res}
 
 uses
-  SysUtils, Classes, Generics.Collections, ConsoleToolbox, JWBIO, UniStrUtils,
-  FilenameUtils;
+  SysUtils, Classes, Generics.Collections, Generics.Defaults, ConsoleToolbox,
+  JWBIO, UniStrUtils, FilenameUtils;
 
 type
+  TExpressionCard = record
+    base: string;
+    deriv: string;
+    procedure Reset;
+  end;
+
   TAnkiDerivatives = class(TCommandLineApp)
   protected
     Files: TFilenameArray;
+    DictFiles: TFilenameArray;
     OutputFile: string;
     Output: TStreamEncoder;
-    Words: TStringList;
+    Words: TDictionary<string, TExpressionCard>;
     function HandleSwitch(const s: string; var i: integer): boolean; override;
     function HandleParam(const s: string; var i: integer): boolean; override;
   public
@@ -33,10 +51,12 @@ type
 
 procedure TAnkiDerivatives.ShowUsage;
 begin
-  writeln('Merges several tag files into one list of words with all tags '
-    +'listed for each.');
+  writeln('Generates derivate cross references for an Anki tag from derivative '
+    +'list files.');
   writeln('Usage: '+ProgramName+' <file1> [file2] ... [-flags]');
   writeln('Flags:');
+  writeln('  -d dictionary     specify dictionary file (multiple -d is allowed,'
+    +' in the priority order)');
   writeln('  -o output.file    specify output file (otherwise console)');
 end;
 
@@ -48,25 +68,58 @@ begin
     OutputFile := ParamStr(i);
     Result := true;
   end else
+  if s='-d' then begin
+    if i>=ParamCount then BadUsage('-d requires dictionary file name');
+    Inc(i);
+    AddFile(DictFiles, ParamStr(i));
+    Result := true;
+  end else
     Result := inherited;
 end;
 
 function TAnkiDerivatives.HandleParam(const s: string; var i: integer): boolean;
 begin
-  SetLength(Files, Length(Files)+1);
-  Files[Length(Files)-1] := s;
+  AddFile(Files, s);
   Result := true;
 end;
 
-//Anki tag list format: space separated ("area jlpt2")
+
+procedure TExpressionCard.Reset;
+begin
+  Self.base := '';
+  Self.deriv := '';
+end;
+
+function _compareString(const Left, Right: string): Boolean;
+begin
+  Result := CompareStr(Left, Right)=0;
+end;
+
+function _hashString(const Value: string): Integer;
+begin
+  if Value='' then
+    Result := 0
+  else
+    Result := BobJenkinsHash(Value[1], Length(Value)*SizeOf(Value[1]), 1234567);
+end;
 
 procedure TAnkiDerivatives.Run;
 var i: integer;
+  pair: TPair<string, TExpressionCard>;
+  ln: string;
 begin
   if ParamCount<1 then BadUsage();
   if Length(Files)<1 then BadUsage('Input files not specified');
 
-  Words := TStringList.Create;
+  Words := TDictionary<string, TExpressionCard>.Create(
+    TDelegatedEqualityComparer<string>.Create(
+      _compareString,
+      _hashString
+    )
+  );
+
+  DictFiles := ExpandFileMasks(DictFiles, true);
+ //TODO: Load all, use to resolve words
 
   Files := ExpandFileMasks(Files, true);
   for i := 0 to Length(Files)-1 do
@@ -78,44 +131,168 @@ begin
     Output := ConsoleWriter;
   Output.WriteBom;
 
-  for i := 0 to Words.Count-1 do
-    Output.WriteLn(Words.Names[i]+#09+Words.ValueFromIndex[i]);
+  for pair in Words do begin
+    Output.Write(pair.Key+#09);
+    if pair.Value.base<>'' then
+      ln := 'От '+pair.Value.base
+    else
+      ln := '';
+    if pair.Value.deriv<>'' then begin
+      if ln<>'' then
+        ln := ln+'; производные: '
+      else
+        ln := 'Производные: ';
+      ln := ln + pair.Value.deriv;
+    end;
+    Output.WriteLn(ln);
+  end;
 
   FreeAndNil(Output);
   FreeAndNil(Words);
 end;
 
+{ Parses one line from a derivative definition file, minding quotes.
+  Does not remove quotes from quoted parts so that you can determine what's
+  what later. }
+function SplitDerivLine(ln: string): TStringArray;
+var beg_i, i: integer;
+  flag_escape: boolean;
+  quote_char: char;
+
+  procedure Commit;
+  var chunk: string;
+  begin
+    if beg_i>=i then exit;
+    chunk := Trim(copy(ln, beg_i, i-beg_i));
+    if chunk<>'' then begin
+      SetLength(Result, Length(Result)+1);
+      Result[Length(Result)-1] := chunk;
+    end;
+    beg_i := i+1;
+  end;
+
+begin
+  SetLength(Result, 0);
+  if ln='' then exit;
+
+  flag_escape := false;
+  quote_char := #00;
+
+  beg_i := 1;
+  i := 1;
+  while i<=Length(ln) do begin
+    if flag_escape then
+      flag_escape := false
+    else
+    if ln[i]='\' then
+      flag_escape := true
+    else
+    if (quote_char='"') and (ln[i]='"') then
+      quote_char := #00
+    else
+    if (quote_char=''') and (ln[i]=''') then
+      quote_char := #00
+    else
+    if quote_char<>#00 then begin
+     //Nothing
+    end else
+    if (ln[i]='"') or (ln[i]='''') then
+      quote_char := ln[i]
+    else
+    if ln[i]=' ' then
+      Commit;
+   //else just a normal char
+   //in any case:
+    Inc(i);
+  end;
+
+  Commit;
+end;
+
+function IsComment(const part: string): boolean;
+begin
+  Result := (Length(part)>=2) and(
+      ((part[1]='"') and (part[Length(part)]='"'))
+   or ((part[1]='''') and (part[Length(part)]=''''))
+  );
+end;
+
+function FormatComment(const part: string): string;
+begin
+  Result := '('+copy(part, 2, Length(part)-2)+')';
+end;
+
+function FindNextComment(const parts: TStringArray; idx: integer): integer;
+var i: integer;
+begin
+  Result := -1;
+  for i := idx to Length(parts)-1 do
+    if IsComment(parts[i]) then begin
+      Result := i;
+      break;
+    end;
+end;
+
+function FormatEntry(const part: string): string;
+begin
+  Result := part; //TODO: Expand to dict entry
+end;
+
 procedure TAnkiDerivatives.ParseFile(const AFilename: string);
 var inp: TStreamDecoder;
-  tagName, ln, list: string;
+  ln: string;
   i: integer;
+  parts: TStringArray;
+  card: TExpressionCard;
+  next_comment: integer;
 begin
-  tagName := ChangeFileExt(ExtractFilename(AFilename), '');
   inp := OpenTextFile(AFilename);
   try
     while inp.ReadLn(ln) do begin
-      ln := Trim(ln);
-      if ln='' then continue;
+      parts := SplitDerivLine(Trim(ln));
+      if Length(parts)<=0 then continue;
 
-     //If reading is provided, accept reading, else use empty reading (for consistence)
-      i := pos(' ', ln);
-      if i>0 then
-        ln[i] := #09
-      else
-        ln := ln + #09;
+     //Base word
+      if not Words.TryGetValue(parts[0], card) then
+        card.Reset;
+      for i := 1 to Length(parts)-1 do begin
+        if IsComment(parts[i]) then begin
+          if card.deriv <> '' then
+            if i=1 then
+              card.deriv := card.deriv + '; '
+            else
+              card.deriv := card.deriv + ' ';
+          card.deriv := card.deriv + FormatComment(parts[i]);
+        end else begin
+          if card.deriv <> '' then
+            if i=1 then
+              card.deriv := card.deriv + '; '
+            else
+              card.deriv := card.deriv + ', ';
+          card.deriv := card.deriv + FormatEntry(parts[i]);
+        end;
+        card.deriv := card.deriv + ''
+      end;
+      Words.AddOrSetValue(parts[0], card);
 
-      i := Words.IndexOfName(ln);
-      if i>=0 then begin
-        list := Words.ValueFromIndex[i];
-        if pos(' '+ln+' ', ' '+list+' ')<=0 then
-          list := list + ' ' + tagName;
-      end else
-        list := tagName;
+     //Derivatives
+      next_comment := FindNextComment(parts, 1);
+      for i := 1 to Length(parts)-1 do begin
+        if IsComment(parts[i]) then begin
+          next_comment := FindNextComment(parts, i+1);
+          continue;
+        end;
 
-      if i>=0 then
-        Words.ValueFromIndex[i] := list
-      else
-        Words.Add(ln+'='+list);
+        if not Words.TryGetValue(parts[i], card) then
+          card.Reset;
+        if card.base <> '' then
+          card.base := card.base + '; ';
+        card.base := card.base + FormatEntry(parts[0]);
+        if next_comment>=0 then
+          card.base := card.base + ' ' + FormatComment(parts[next_comment]);
+        Words.AddOrSetValue(parts[i], card);
+      end;
+
     end;
 
   finally
